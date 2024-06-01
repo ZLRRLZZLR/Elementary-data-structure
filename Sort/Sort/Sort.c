686eaf592d55d34fb02bdda03d1b13ff4e5b0fd6
zhou-liurong <19551230598@163.com>
2024-06-01 23:10:59 +0800

    sort

--- Sort/Sort/Sort.c
+++ Sort/Sort/Sort.c
@@ -17,30 +17,30 @@ void SelectSort(int* a, int n)
 	int end = n - 1;
 	while(begin < end)
 	{
-		int max = a[begin + 1];
-		int min = a[begin + 1];
-
+		int max = begin;
+		int min = begin;
 		for (int i = begin + 1; i <= end; i++)
 		{
-			if(begin == 0)
+
+
+			if (a[i] > a[max])
 			{
-				max = a[0];
-				min = a[0];
+				max = i;
 			}
 
-			if (a[i] > max)
+			if (a[i] < a[min])
 			{
-				max = a[i];
-			}
-
-			if (a[i] < min)
-			{
-				min = a[i];
+				min = i;
 			}
 		}
-		Swap(&a[begin], &min);
-		Swap(&a[end], &max);
-		++begin;
+
+		Swap(&a[begin], &a[min]);
+		if (max == begin)
+		{
+			max = min;
+		}
+		Swap(&a[end], &a[max]);
+   		++begin;
 		--end;
 	}
 }
--- Sort/Sort/test.c
+++ Sort/Sort/test.c
@@ -2,7 +2,8 @@
 
 int main()
 {
-	int a[] = { 2,4,8,3,1,7,9,2 };
+	//int a[] = { 2,4,8,3,1,7,9,2 };
+	int a[] = { 9,1,2,5,7,4,6,3};
 	// —°‘Ò≈≈–Ú
 	SelectSort(a, sizeof(a) / sizeof(int));
 
