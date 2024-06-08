2c3e4c7268d6eb291652299f577903ae269fc24c
zhou-liurong <19551230598@163.com>
2024-06-08 23:35:36 +0800

    Quick scheduling and its optimization

--- Sort/Sort/Sort.c
+++ Sort/Sort/Sort.c
@@ -194,10 +194,41 @@ void BubbleSort(int* a, int n)
 }
 
 //三数取中
-int Gitmid(int* a, int left, int right);
+int GetMid(int* a, int left, int right)
 {
-	int key = (right - left) / 2;
-	if()
+	int mid = left + (left + right)>>1;
+
+	if(a[left] > a[mid])
+	{
+		if(a[mid] > a[right])
+		{
+			return mid;
+		}
+		else if(a[left] > a[right])
+			{
+				return right;
+			}
+		else
+			{
+				return left;
+			}
+	}
+	else
+	{
+		if(a[mid] < a[right])
+		{
+			return mid;
+		}
+		else if (a[left] > a[right])
+			{
+				return left;
+			}
+			else
+			{
+				return right;
+			}
+		
+	}
 
 }
 
@@ -207,22 +238,172 @@ int Gitmid(int* a, int left, int right);
 // 快速排序hoare版本
 int PartSort1(int* a, int left, int right)
 {
-	int key = Gitmid(a,left,right);
+
+	int key = GetMid(a,left,right);
+	Swap(&a[key],&a[left]);
+
 	int begin = left;
 	int end = right;
-	while(a[end] < )
-	{
 
+	while(begin < end)
+	{
+		while (begin < end && a[end] >= a[left])
+		{
+			--end;
+		}
+		while(begin < end && a[begin] <= a[left])
+		{
+			++begin;
+		}
+
+		Swap(&a[begin], &a[end]);
 	}
+
+	Swap(&a[left],&a[begin]);
+
+	return begin;
+
 }
 
 // 快速排序挖坑法
-int PartSort2(int* a, int left, int right);
+int PartSort2(int* a, int left, int right)
+{
+	int key = GetMid(a, left, right);
+	Swap(&a[key], &a[left]);
+
+	key = a[left];
+
+	int begin = left;
+	int end = right;
+
+	while (begin < end)
+	{
+		while (begin < end && a[end] >= key)
+		{
+			--end;
+		}
+
+		a[begin] = a[end];
+
+		while (begin < end && a[begin] <= key)
+		{
+			++begin;
+		}
+
+		a[end] = a[begin];
+
+	}
+
+	a[begin] = key;
+
+	return begin;
+}
 
 // 快速排序前后指针法
-int PartSort3(int* a, int left, int right);
+int PartSort3(int* a, int left, int right)
+{
+	int prev = left;
+	int cur = left + 1;
 
-void QuickSort(int* a, int left, int right);
+	int key = GetMid(a, left, right);
+	Swap(&a[key], &a[left]);
+
+	//while(cur <= right)
+	//{
+	//	if(a[cur] < a[left] && ++prev != cur)
+	//	{
+	//		Swap(&a[cur],&a[prev]);
+	//	}
+
+	//	cur++;
+	//}
+	while(cur <= right)
+	{
+		while(cur <= right && a[cur] > a[left])
+		{
+			cur++;
+		}
+		prev++;
+		if(prev != cur)
+		{
+			Swap(&a[prev],&a[cur]);
+		}
+		cur++;
+	}
+
+	Swap(&a[prev], &a[left]);
+
+	return prev;
+}
+
+void QuickSort(int* a, int left, int right)
+{
+	if (left >= right)
+		return;
+
+	if(right - left + 1 <= 10)
+	{
+		InsertSort(a + left, right - left + 1);
+	}
+
+	else
+	{
+
+		//int key = PartSort1(a, left, right);
+		//int key = PartSort2(a, left, right);
+		int key = PartSort3(a, left, right);
+
+
+		QuickSort(a, left, key - 1);
+		QuickSort(a, key + 1, right);
+	}
+
+}
+#include"Stack.h"
 
 // 快速排序 非递归实现
-void QuickSortNonR(int* a, int left, int right);
+void QuickSortNonR(int* a, int left, int right)
+{
+
+		Stack q;
+		StackInit(&q);
+		if(left < right)
+		{
+			StackPush(&q, right);
+			StackPush(&q, left);
+		}
+
+
+		while (!StackEmpty(&q))
+		{
+			int begin = StackTop(&q);
+			StackPop(&q);
+			int end = StackTop(&q);
+			StackPop(&q);
+
+			if (right - left + 1 <= 10)
+			{
+				InsertSort(a + left, right - left + 1);
+			}
+
+			else
+			{
+				int key = PartSort1(a, begin, end);
+
+				if (key + 1 < end)
+				{
+					StackPush(&q, end);
+					StackPush(&q, key + 1);
+				}
+
+				if (begin < key - 1)
+				{
+					StackPush(&q, key - 1);
+					StackPush(&q, begin);
+				}
+			}
+
+		}
+		StackDestroy(&q);
+
+}
--- Sort/Sort/test.c
+++ Sort/Sort/test.c
@@ -1,5 +1,12 @@
 #include"Sort.h"
 
+void Print(int*a)
+{
+	for (int i = 0; i < 14; i++)
+	{
+		printf("%2d ", a[i]);
+	}
+}
 
 int main()
 {
@@ -7,16 +14,30 @@ int main()
 	int a[] = { 9,1,2,5,7,4,6,3,10,6,9,15,12,-1};
 	// Ñ¡ÔñÅÅÐò
 	//SelectSort(a, sizeof(a) / sizeof(int));
-	/*InsertSort(a, sizeof(a) / sizeof(int));*/
+	//Print(a);
+	//printf("\n");
+	//InsertSort(a, sizeof(a) / sizeof(int));
+	//Print(a);
+	//printf("\n");
 	//ShellSort(a, sizeof(a) / sizeof(int));
-	//AdjustDwon(a, sizeof(a) / sizeof(int),0);
-	//HeapSort(a, sizeof(a) / sizeof(int), 0);
-	BubbleSort(a, sizeof(a) / sizeof(int));
+	//Print(a);
 
-	for (int i = 0; i < sizeof(a) / sizeof(int); i++)
-	{
-		printf("%d ", a[i]);
-	}
+	//printf("\n");
+	////AdjustDwon(a, sizeof(a) / sizeof(int),0);
+	//HeapSort(a, sizeof(a) / sizeof(int), 0);
+	//Print(a);
+	//printf("\n");
+
+	//BubbleSort(a, sizeof(a) / sizeof(int));
+	//Print(a);
+	//printf("\n");
+	QuickSort(a,0, sizeof(a) / sizeof(int) - 1);
+	Print(a);
+	//printf("\n");
+	//QuickSortNonR(a, 0, sizeof(a) / sizeof(int) - 1);
+	//Print(a);
+	//printf("\n");
+
 
 
 	return 0;
