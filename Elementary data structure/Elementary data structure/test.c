eb6f890ab8a7c34a78fa75c84960edf7c933f60b
zhou-liurong <19551230598@163.com>
2024-06-14 23:12:57 +0800

    review

--- Elementary data structure/Elementary data structure/test.c
+++ Elementary data structure/Elementary data structure/test.c
@@ -5,6 +5,58 @@
 #include<assert.h>
 #include<stdio.h>
 
+
+///**环形链表二
+// * Definition for singly-linked list.
+// * struct ListNode {
+// *     int val;
+// *     struct ListNode *next;
+// * };
+// */
+//struct ListNode* detectCycle(struct ListNode* head) {
+//    struct ListNode* slow = head;
+//    struct ListNode* fast = head;
+//    while (fast && fast->next)
+//    {
+//        fast = fast->next->next;
+//        slow = slow->next;
+//        if (slow == fast)
+//        {
+//            struct ListNode* meet = slow;
+//            while (head != meet)
+//            {
+//                head = head->next;
+//                meet = meet->next;
+//            }
+//            return meet;
+//        }
+//    }
+//
+//    return NULL;
+//
+//}
+///**环形链表
+// * Definition for singly-linked list.
+// * struct ListNode {
+// *     int val;
+// *     struct ListNode *next;
+// * };
+// */
+//bool hasCycle(struct ListNode* head) {
+//    struct ListNode* slow = head;
+//    struct ListNode* fast = head;
+//    while (fast && fast->next)
+//    {
+//        fast = fast->next->next;
+//        slow = slow->next;
+//        if (slow == fast)
+//        {
+//            return true;
+//        }
+//    }
+//    return false;
+//
+//}
 ///**合并两个有序链表
 // * Definition for singly-linked list.
 // * struct ListNode {
