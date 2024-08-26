462b5e05e937f8001ce1984fe4b120e10992d91f
zhou-liurong <19551230598@163.com>
2024-08-26 22:50:54 +0800

    review

--- Heap/Heap/Heap.c
+++ Heap/Heap/Heap.c
@@ -1,166 +1,166 @@
-#include"Heap.h"
-
-
-//堆的初始化
-void HeapInit(HP* php)
-{
-	assert(php);
-	php->_a = NULL;
-	php->_capacity = php->_size = 0;
-
-}
-
-// 堆的销毁
-void HeapDestory(HP* hp)
-{
-
-	assert(hp);
-	free(hp->_a);
-	hp->_a = NULL;
-	hp->_capacity = hp->_size = 0;
-
-}
-
-//交换
-void Swap(HPDataType* a, HPDataType* b)
-{
-	HPDataType temp = *a;
-	*a = *b;
-	*b = temp;
-
-}
-
-//向上调整算法
-void AdjustUp(HPDataType* a, int child)
-{
-	assert(a);
-
-	int parent = (child - 1) / 2;
-	while (child > 0)
-	{
-		if (a[child] > a[parent])
-		{
-			Swap(&a[child], &a[parent]);
-			child = parent;
-			parent = (child - 1) / 2;
-		}
-
-		else
-		{
-
-			break;
-
-		}
-	}
-}
-
-// 堆的插入
-void HeapPush(HP* hp, HPDataType x)
-{
-	assert(hp);
-	if (hp->_capacity == hp->_size)
-	{
-		int newcapacity = hp->_capacity == 0 ? 4 : 2 * hp->_capacity;
-		HPDataType* newnode = (HPDataType*)realloc(hp->_a, newcapacity * sizeof(HPDataType));
-
-		if (NULL == newnode)
-		{
-			perror("StackInit:realloc");
-			exit(1);
-		}
-		hp->_a = newnode;
-		hp->_capacity = newcapacity;
-	}
-
-	hp->_a[hp->_size] = x;
-	hp->_size++;
-	AdjustUp(hp->_a, hp->_size - 1);
-
-}
-
-//向下调整算法
-void AdjustDown(HPDataType* a, int n, int parent)
-{
-	assert(a);
-	int child = parent * 2 + 1;
-	while (child < n)
-	{
-		if(child + 1 < n && a[child] < a[child + 1])
-		{
-			child = child + 1;
-		}
-		if(a[child] > a[parent])
-		{
-			Swap(&a[child], &a[parent]);
-			parent = child;
-			child = parent * 2 + 1;
-		}
-		else
-		{
-			break;
-		}
-	}
-
-} 
-
-// 堆的删除
-void HeapPop(HP* hp)
-{
-	assert(hp);
-	assert(hp->_size > 0);
-	Swap(&hp->_a[0],&hp->_a[hp->_size - 1]);
-
-	hp->_size--;
-
-	AdjustDown(hp->_a, hp->_size, 0);
-
-}
-
-// 取堆顶的数据
-HPDataType HeapTop(HP* hp)
-{
-	assert(hp);
-	assert(hp->_size > 0);
-
-	return hp->_a[0];
-}
-
-// 堆的数据个数
-int HeapSize(HP* hp)
-{
-	assert(hp);
-	return hp->_size;
-}
-
-// 堆的判空
-int HeapEmpty(HP* hp)
-{
-
-	assert(hp);
-	return hp->_size == 0;
-
-}
-
-// 对数组进行堆排序
-void HeapSort(int* a, int n)
-{
-	//for(int i = 1;i < n;i++)
-	//{
-	//	AdjustUp(a, i);
-	//}
-
-	for(int i = (n - 2) / 2;i >= 0;i--)
-	{
-
-		AdjustDown(a,n,i);
-
-	}
-
-	int end = n - 1;
-	while(end > 0)
-	{
-		Swap(&a[0], &a[end]);
-		AdjustDown(a, end, 0);
-		--end;
-	}
-}
+//#include"Heap.h"
+//
+//
+////堆的初始化
+//void HeapInit(HP* php)
+//{
+//	assert(php);
+//	php->_a = NULL;
+//	php->_capacity = php->_size = 0;
+//
+//}
+//
+//// 堆的销毁
+//void HeapDestory(HP* hp)
+//{
+//
+//	assert(hp);
+//	free(hp->_a);
+//	hp->_a = NULL;
+//	hp->_capacity = hp->_size = 0;
+//
+//}
+//
+////交换
+//void Swap(HPDataType* a, HPDataType* b)
+//{
+//	HPDataType temp = *a;
+//	*a = *b;
+//	*b = temp;
+//
+//}
+//
+////向上调整算法
+//void AdjustUp(HPDataType* a, int child)
+//{
+//	assert(a);
+//
+//	int parent = (child - 1) / 2;
+//	while (child > 0)
+//	{
+//		if (a[child] > a[parent])
+//		{
+//			Swap(&a[child], &a[parent]);
+//			child = parent;
+//			parent = (child - 1) / 2;
+//		}
+//
+//		else
+//		{
+//
+//			break;
+//
+//		}
+//	}
+//}
+//
+//// 堆的插入
+//void HeapPush(HP* hp, HPDataType x)
+//{
+//	assert(hp);
+//	if (hp->_capacity == hp->_size)
+//	{
+//		int newcapacity = hp->_capacity == 0 ? 4 : 2 * hp->_capacity;
+//		HPDataType* newnode = (HPDataType*)realloc(hp->_a, newcapacity * sizeof(HPDataType));
+//
+//		if (NULL == newnode)
+//		{
+//			perror("StackInit:realloc");
+//			exit(1);
+//		}
+//		hp->_a = newnode;
+//		hp->_capacity = newcapacity;
+//	}
+//
+//	hp->_a[hp->_size] = x;
+//	hp->_size++;
+//	AdjustUp(hp->_a, hp->_size - 1);
+//
+//}
+//
+////向下调整算法
+//void AdjustDown(HPDataType* a, int n, int parent)
+//{
+//	assert(a);
+//	int child = parent * 2 + 1;
+//	while (child < n)
+//	{
+//		if(child + 1 < n && a[child] < a[child + 1])
+//		{
+//			child = child + 1;
+//		}
+//		if(a[child] > a[parent])
+//		{
+//			Swap(&a[child], &a[parent]);
+//			parent = child;
+//			child = parent * 2 + 1;
+//		}
+//		else
+//		{
+//			break;
+//		}
+//	}
+//
+//} 
+//
+//// 堆的删除
+//void HeapPop(HP* hp)
+//{
+//	assert(hp);
+//	assert(hp->_size > 0);
+//	Swap(&hp->_a[0],&hp->_a[hp->_size - 1]);
+//
+//	hp->_size--;
+//
+//	AdjustDown(hp->_a, hp->_size, 0);
+//
+//}
+//
+//// 取堆顶的数据
+//HPDataType HeapTop(HP* hp)
+//{
+//	assert(hp);
+//	assert(hp->_size > 0);
+//
+//	return hp->_a[0];
+//}
+//
+//// 堆的数据个数
+//int HeapSize(HP* hp)
+//{
+//	assert(hp);
+//	return hp->_size;
+//}
+//
+//// 堆的判空
+//int HeapEmpty(HP* hp)
+//{
+//
+//	assert(hp);
+//	return hp->_size == 0;
+//
+//}
+//
+//// 对数组进行堆排序
+//void HeapSort(int* a, int n)
+//{
+//	//for(int i = 1;i < n;i++)
+//	//{
+//	//	AdjustUp(a, i);
+//	//}
+//
+//	for(int i = (n - 2) / 2;i >= 0;i--)
+//	{
+//
+//		AdjustDown(a,n,i);
+//
+//	}
+//
+//	int end = n - 1;
+//	while(end > 0)
+//	{
+//		Swap(&a[0], &a[end]);
+//		AdjustDown(a, end, 0);
+//		--end;
+//	}
+//}
--- Heap/Heap/Heap.h
+++ Heap/Heap/Heap.h
@@ -4,42 +4,99 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+//typedef int HPDataType;
+//typedef struct Heap
+//{
+//	HPDataType* _a;
+//	int _size;
+//	int _capacity;
+//}HP;
+//
+////堆的初始化
+//void HeapInit(HP* php);
+//
+////堆排序
+//void HeapSort(HPDataType* a, int n);
+//
+//// 堆的销毁
+//void HeapDestory(HP* hp);
+//
+//// 堆的插入
+//void HeapPush(HP* hp, HPDataType x);
+//
+////向上调整算法
+//void AdjustUp(HPDataType* a, int child);
+//
+////向下调整算法
+//void AdjustDown(HPDataType* a, int n, int parent);
+//
+////交换
+//void Swap(int* a, int* b);
+//
+//// 堆的删除
+//void HeapPop(HP* hp);
+//
+//// 取堆顶的数据
+//HPDataType HeapTop(HP* hp);
+//
+//// 堆的数据个数
+//int HeapSize(HP* hp);
+//
+//// 堆的判空
+//int HeapEmpty(HP* hp);
+
 typedef int HPDataType;
 typedef struct Heap
 {
 	HPDataType* _a;
 	int _size;
 	int _capacity;
-}HP;
+}Heap;
 
 //堆的初始化
-void HeapInit(HP* php);
-
-void HeapSort(int* a, int n);
+void HeapInit(Heap* php);
 
 // 堆的销毁
-void HeapDestory(HP* hp);
+void HeapDestory(Heap* hp);
 
 // 堆的插入
-void HeapPush(HP* hp, HPDataType x);
-
-//向上调整算法
-void AdjustUp(HPDataType* a, int child);
-
-//向下调整算法
-void AdjustDown(HPDataType* a, int n, int parent);
-
-//交换
-void Swap(int* a, int* b);
+void HeapPush(Heap* hp, HPDataType x);
 
 // 堆的删除
-void HeapPop(HP* hp);
+void HeapPop(Heap* hp);
 
 // 取堆顶的数据
-HPDataType HeapTop(HP* hp);
+HPDataType HeapTop(Heap* hp);
 
 // 堆的数据个数
-int HeapSize(HP* hp);
+int HeapSize(Heap* hp);
 
 // 堆的判空
-int HeapEmpty(HP* hp);
+int HeapEmpty(Heap* hp);
+
+// 对数组进行堆排序
+void HeapSort(int* a, int n);
+
+//void CreateNDate()
+//{
+//	// 造数据
+//	int n = 10000;
+//	srand(time(0));
+//	const char* file = "data.txt";
+//	FILE* fin = fopen(file, "w");
+//	if (fin == NULL)
+//	{
+//		perror("fopen error");
+//		return;
+//	}
+//
+//	for (size_t i = 0; i < n; ++i)
+//	{
+//		int x = rand() % 1000000;
+//		fprintf(fin, "%d\n", x);
+//	}
+//
+//	fclose(fin);
+//}
+//
+//void PrintTopK(int k);
