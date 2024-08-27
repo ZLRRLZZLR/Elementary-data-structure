6ff88a7d86c413fba24ce199ae24f35ef8e00dc0
zhou-liurong <19551230598@163.com>
2024-08-27 20:53:42 +0800

    review

--- Heap/Heap/Heap.h
+++ Heap/Heap/Heap.h
@@ -1,9 +1,9 @@
-#pragma once
-#define _CRT_SECURE_NO_WARNINGS 1
-#include<assert.h>
-#include<stdio.h>
-#include<stdlib.h>
-
+//#pragma once
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<assert.h>
+//#include<stdio.h>
+//#include<stdlib.h>
+//
 //typedef int HPDataType;
 //typedef struct Heap
 //{
@@ -44,59 +44,4 @@
 //
 //// 堆的判空
 //int HeapEmpty(HP* hp);
-
-typedef int HPDataType;
-typedef struct Heap
-{
-	HPDataType* _a;
-	int _size;
-	int _capacity;
-}Heap;
-
-//堆的初始化
-void HeapInit(Heap* php);
-
-// 堆的销毁
-void HeapDestory(Heap* hp);
-
-// 堆的插入
-void HeapPush(Heap* hp, HPDataType x);
-
-// 堆的删除
-void HeapPop(Heap* hp);
-
-// 取堆顶的数据
-HPDataType HeapTop(Heap* hp);
-
-// 堆的数据个数
-int HeapSize(Heap* hp);
-
-// 堆的判空
-int HeapEmpty(Heap* hp);
-
-// 对数组进行堆排序
-void HeapSort(int* a, int n);
-
-//void CreateNDate()
-//{
-//	// 造数据
-//	int n = 10000;
-//	srand(time(0));
-//	const char* file = "data.txt";
-//	FILE* fin = fopen(file, "w");
-//	if (fin == NULL)
-//	{
-//		perror("fopen error");
-//		return;
-//	}
 //
-//	for (size_t i = 0; i < n; ++i)
-//	{
-//		int x = rand() % 1000000;
-//		fprintf(fin, "%d\n", x);
-//	}
-//
-//	fclose(fin);
-//}
-//
-//void PrintTopK(int k);
